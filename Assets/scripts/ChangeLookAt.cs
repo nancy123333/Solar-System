f5ae2830ba5e4113d3d1f389abe1b8a078c3adb9
nancy123333 <nancysebip@gmail.com>
2023-09-02 23:55:56 +0530

    Solar System

---
+++ Assets/scripts/ChangeLookAt.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLookAt : MonoBehaviour
+{
+    // Declare a game object variable which which defines the target where the camera should look at
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(target == null) { 
+        target = this.gameObject;
+        Debug.Log("Look at target target not specified. defaulting to parent object.");
+           
+        }
+
+        
+        
+           
+    }
+
+    // Update is called once per frame
+    private void OnMouseDown() {
+    LookAt.target = target;
+    Camera.main.fieldOfView =Mathf.Clamp(60 * target.transform.localScale.x, 1, 100);
+
+
+    }
+}
